e8c52347b0af1f4fc6993dcd07aad0aa5b67d651
Amir Talkanov <amir.talkanov@yandex.ru>
2024-01-14 14:28:12 +0300

    Добавить задания из видеоурока 3 раздела

---
+++ third_task/ProjectSwitch/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            switch (n)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Понедельник");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Вторник");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Среда");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Четверг");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Пятница");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Суббота");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Воскресенье");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Ошибка!");
+                    }
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
