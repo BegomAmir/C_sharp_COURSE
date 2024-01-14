d5b800b48d46445881c21fa37329bc4e867bb04f
Amir Talkanov <amir.talkanov@yandex.ru>
2024-01-14 20:25:55 +0300

    Добавление решения From_videos в репозиторий

---
+++ From_videos/Sphere/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sphere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double v = ((double)4) / 3 * Math.PI * Math.Pow(r, 3);
+            Console.WriteLine(v);
+            Console.ReadKey();
+        }
+    }
+}
