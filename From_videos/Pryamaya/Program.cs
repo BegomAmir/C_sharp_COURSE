c40a0266bb3344d925d8cf1dde4b3a464510bdfe
Amir Talkanov <amir.talkanov@yandex.ru>
2024-01-14 22:58:26 +0300

    Добавить в решение новый проект

---
+++ From_videos/Pryamaya/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pryamaya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координату первой точки");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите координату второй точки");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            if (x1 == x2 || y1 == y2)
+            {
+                Console.WriteLine("Точки лежат на прямой, параллельной Ох или Оу");
+            }
+            else 
+            {
+                Console.WriteLine("Точки не лежат на прямой, параллельной Ох или Оу");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ From_videos/griby/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace griby
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите N");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int o = n % 10;
+            if (n % 100 >= 11 && n % 100 <= 19)
+            {
+                Console.WriteLine("Мы нашли {0} грибов", n);
+            }
+            else
+            {
+                switch (o)
+                {
+                    case 1:
+                        Console.WriteLine("Мы нашли {0} гриб", n);
+                        break;
+                    case 2:
+                    case 3:
+                    case 4:
+                        Console.WriteLine("Мы нашли {0} гриба", n);
+                        break;
+                    default:
+                        Console.WriteLine("Мы нашли {0} грибов", n);
+                        break;
+
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
