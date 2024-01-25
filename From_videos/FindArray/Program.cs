292406e49dd01fb6496d7bd9e6ceadb38741800b
Amir Talkanov <amir.talkanov@yandex.ru>
2024-01-25 19:02:56 +0300

    Добавить решение задач из видео 5

---
+++ From_videos/FindArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace FindArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = random.Next(0, 10);
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            int Max = array[0];
+            foreach (int a in array)
+            {
+                if (a > Max)
+                    Max = a;
+            }
+            Console.WriteLine(Max);
+            Console.ReadKey();
+        }
+    }
+}
