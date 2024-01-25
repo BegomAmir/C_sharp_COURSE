1d4487b94c082dd856af6f3eb2d7cb0552db8a3e
Amir Talkanov <amir.talkanov@yandex.ru>
2024-01-25 15:51:02 +0300

    Добавить решение 3 задачи

---
+++ fourth_task/problem3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите A");
+            double A = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите B");
+            double B = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите C");
+            double C = Convert.ToDouble(Console.ReadLine());
+            int x = 0;
+            double t = B;
+            while (C <= A)
+            {
+                while (C <= B)
+                {
+                    x++;
+                    B = B - C;
+                }
+                A = A - C;
+                B = t;
+            }
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
