81a201d27b0a69b02d941e72b0908045d512e5b8
Amir Talkanov <amir.talkanov@yandex.ru>
2024-01-26 23:28:46 +0300

    Добавить решение второй задачи

---
+++ fifth_task/problem2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[15];
+            Random random = new Random();
+            for (int i = 0; i < 15; i++)
+            {
+                array[i] = random.Next(0,50);
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            int Max = array[0];
+            int Min = array[0];
+            foreach (int a in array)
+            {
+                if (a > Max)
+                {
+                    Max = a;
+                }
+            }
+            foreach (int b in array)
+            {
+                if (b < Min)
+                {
+                    Min = b;
+                }
+            }
+            int Sum = Max + Min;
+            Console.WriteLine("Наибольший член равен {0}", Max);
+            Console.WriteLine("Наименьший член равен {0}", Min);    
+            Console.WriteLine("Сумма равна {0}", Sum);
+            Console.ReadKey();
+        }
+    }
+}
