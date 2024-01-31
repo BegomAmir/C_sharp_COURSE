c2b3c6bdd32bed6d4106f2821d86bd5daf506483
Amir Talkanov <amir.talkanov@yandex.ru>
2024-01-31 22:58:09 +0300

    Добавить вторую задачу из урока 6

---
+++ From_videos/6.2.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+            string str = Console.ReadLine();
+            string str2 = "";
+            foreach (char c in str) 
+            {
+                str2 = c + str2;
+            }
+            Console.WriteLine(str2);
+            Console.ReadKey();
+        }
+    }
+}
