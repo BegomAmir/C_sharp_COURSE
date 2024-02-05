0a9f366e60f82b5dd52f7e0c152c67301df946d0
Amir Talkanov <amir.talkanov@yandex.ru>
2024-02-05 18:18:27 +0300

    Добавить решение второй задачи

---
+++ seventh_task/problem2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem2
+{
+    internal class Program
+    {
+        static void getCube(double a, out double s, out double v)
+        {
+            s = 6 * a * a;
+            v = a * a * a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону куба");
+            double a = Convert.ToDouble(Console.ReadLine());    
+            double s, v;
+            getCube(a, out s, out v);
+            Console.WriteLine("Площадь поверхности равна {0:.000}, а объём равен {1:.000}", s, v);
+            Console.ReadKey();
+
+        }
+    }
+}
