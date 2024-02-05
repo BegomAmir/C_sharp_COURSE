6686bf66adb1534a3e896e0146ea0a547c7cf54f
Amir Talkanov <amir.talkanov@yandex.ru>
2024-02-05 16:48:37 +0300

    Добавить решение задач из раздела 7

---
+++ From_videos/7.2.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2._1
+{
+    internal class Program
+    {
+        static double getHypotenuse(double a, double b)
+        {
+            return Math.Sqrt(a * a + b * b);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите катеты первого треугольника");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double z1 = getHypotenuse(x1,y1);
+            Console.WriteLine("Введите катеты второго треугольника");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double z2 = getHypotenuse(x2, y2);
+
+            if (z1 > z2)
+                Console.WriteLine("Гипотегнуза первого треугольника больше");
+            else
+                if (z1 < z2)
+                Console.WriteLine("Гипотегнуза второго треугольника больше");
+            else
+                Console.WriteLine("Гипотенузы равны");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ From_videos/7.2.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2._2
+{
+    internal class Program
+    {
+        static void getParamsSphere(double r, out double s, out double v)
+        {
+            s = 4 * Math.PI * r * r;
+            v = 4.0 / 3 * Math.PI * r * r * r;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double v;
+            getParamsSphere(r, out s, out v);
+            Console.WriteLine("{0:.000} {1:.000}", s,v);
+            Console.ReadKey();
+
+
+        }
+    }
+}
