0f16392e1d7c58e16688778100ee0479c1cb898c
Amir Talkanov <amir.talkanov@yandex.ru>
2024-02-05 18:55:30 +0300

    Добавить задачу из видеоурока 8

---
+++ From_videos/task8.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Logs2";
+            //if (!Directory.Exists(path))
+            //{
+            //    Directory.CreateDirectory(path);
+            //}
+            DirectoryInfo directory = new DirectoryInfo(path); 
+            if (!directory.Exists ) 
+            { 
+                directory.Create();
+            }
+        }
+    }
+}
