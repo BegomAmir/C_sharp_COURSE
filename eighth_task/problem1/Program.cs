8fe051bcbfabdf8e44b80276cfa9a9f4928397f5
Amir Talkanov <amir.talkanov@yandex.ru>
2024-02-06 18:52:50 +0300

    Добавить первую задачу

---
+++ eighth_task/problem1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\8.1.1";
+            string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
