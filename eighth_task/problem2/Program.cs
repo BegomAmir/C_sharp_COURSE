1414022fd374616dad1d9787bace972aa97f7b52
Amir Talkanov <amir.talkanov@yandex.ru>
2024-02-06 19:21:12 +0300

    Добавить вторую задачу

---
+++ eighth_task/problem2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\8.1.1\pomp.txt";
+            Random random = new Random();
+            using(StreamWriter sw = new StreamWriter(path))
+            {
+                for (int i = 0; i <10; i++)
+                {
+                    sw.WriteLine(random.Next(-100,100));
+                }
+            }
+            int k = 0;
+            using(StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i<10; i++)
+                {
+                    k += Convert.ToInt32(sr.ReadLine());
+                }
+            }
+            Console.WriteLine(k);
+            Console.ReadKey();
+        }
+    }
+}
