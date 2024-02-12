6aeb5de86cc19413a736937186978ac691669c47
Amir Talkanov <amir.talkanov@yandex.ru>
2024-02-12 22:18:41 +0300

    Добавить файлы из урока 11

--- From_videos/10.1/Program.cs
+++ From_videos/10.1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using video11;
 
 namespace _10._1
 {
@@ -10,45 +11,10 @@ namespace _10._1
     {
         static void Main(string[] args)
         {
-            Cat cat = new Cat("Мурка", "черный", 1 );
+            Cat cat = new Cat("Мурка", "черный", 1);
             cat.Say();
             Console.ReadKey();
         }
-    }
-    class Cat
-    {
-        public string Name { get; set; }
-        public string Color { get; set; }
-        int age;
 
-        public int Age
-        {
-            set
-            {
-                if (value > 0)
-                {
-                    this.age = value;
-                }
-                else
-                {
-                    Console.WriteLine("Возраст не должен быть отрицательным");
-                }
-            }
-            get
-            {
-                return age;
-            }
-        } 
-        public Cat(string name, string color, int age)
-        {
-            Name = name;
-            Color = color;
-            Age = age;
-        }
-
-        public void Say() 
-        {
-            Console.WriteLine("Мяу. Меня зовут {0}. У меня {1} цвет. Мне {2} лет", Name, Color, Age);
-        }
     }
 }
---
+++ video11/structCat/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structCat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat;
+            cat.name = "Мурка";
+            cat.color = "Черный";
+            cat.age = 1;
+            cat.Say();
+        }
+        struct Cat
+        {
+            public string name;
+            public string color;
+            public int age;
+            public void Say()
+            {
+                Console.WriteLine("Мяу");
+            }
+        }
+    }
+}
