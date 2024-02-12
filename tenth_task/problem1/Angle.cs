5fc71ce2a1dd17fb17e544a2a54e925d8a37664b
Amir Talkanov <amir.talkanov@yandex.ru>
2024-02-12 18:56:01 +0300

    Добавить реализацию класса и решение задачи

---
+++ tenth_task/problem1/Angle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    internal class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public bool isCorrect = true;
+
+        public Angle(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value > 360 || value < 0)
+                    isCorrect = false;
+                else
+                    gradus = value;
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value > 60 || value < 0)
+                    isCorrect = false;
+                else
+                    min = value / 60;
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value > 60 || value < 0)
+                    isCorrect = false;
+                else
+                    sec = value / 3600;
+            }
+        }
+        public double ToRadian()
+        {
+            double radian = (gradus + min + sec) * 3.1415926535 / 180;
+            return radian;
+        }
+    }
+}
---
+++ tenth_task/problem1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle;
+            int i = 0;
+
+            Console.WriteLine("Введите градусы, минуты, секунды по порядку");
+            do
+            {
+                if (i > 0)
+                {
+                    Console.WriteLine("Данные неверны, введите еще раз");
+                }
+                i++;
+                int gradus = Convert.ToInt32(Console.ReadLine());
+                int min = Convert.ToInt32(Console.ReadLine());
+                int sec = Convert.ToInt32(Console.ReadLine());
+                 
+                angle = new Angle(gradus, min, sec);
+
+            } while (angle.isCorrect == false);
+            double radianAngle = angle.ToRadian();
+            Console.WriteLine("{0} - угол в радианах", radianAngle);
+            Console.ReadKey();
+        }
+    }
+}
