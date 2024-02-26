8bbe9df3682ee62abe3211632de50439b2446e28
Amir Talkanov <amir.talkanov@yandex.ru>
2024-02-26 17:29:49 +0300

    Добавить решение одиннадцатого задания

---
+++ eleventh_task/problem1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите коэффициенты уравнения");
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            linear linear = new linear(k,b);
+            Console.WriteLine(linear.Root());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ eleventh_task/problem1/linear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    struct linear
+    {
+        double k;
+        double b;
+
+        public linear(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            if (k == 0)
+                return "Решений нет";
+            double x = -b / k;
+            return $"{x} является решением уравнения";
+        }
+    }
+}
