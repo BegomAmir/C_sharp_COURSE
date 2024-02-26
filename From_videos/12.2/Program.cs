d0a16dc3233c46233d290b7fa970c4350de8df16
Amir Talkanov <amir.talkanov@yandex.ru>
2024-02-26 18:20:35 +0300

    Добавить задание из второго видео темы 12

---
+++ From_videos/12.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double square = Rectangle.getSquare(a, b);
+            double perimetr = Rectangle.getPerimetr(a, b);
+            Console.WriteLine($"{square} \n{perimetr}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ From_videos/12.2/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._2
+{
+    static class Rectangle
+    {
+        static public double getSquare(double a, double b)
+        {
+            return a * b;
+        }
+        static public double getPerimetr(double a, double b)
+        {
+            return 2 * (a + b);
+        }
+    }
+}
