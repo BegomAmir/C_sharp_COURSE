61b9813b2360573deeceff4c7d83f53e0a9ea03f
Amir Talkanov <amir.talkanov@yandex.ru>
2024-02-26 18:41:19 +0300

    Добавить решение задачи из темы 12

---
+++ twelfth_task/problem1/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    static class Circle
+    {
+        static public double getLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double getSquare(double r)
+        {
+            return Math.PI * r * r;
+        }
+        static public void checkCoordinate(double x, double y, double r, double x0, double y0)
+        {
+            if (x0 + r >= x && y0 + r >= y)
+            {
+                 Console.WriteLine("Точка принадлежит окружности");
+            }
+            else
+            {
+                Console.WriteLine("Точка не принадлежит окружности");
+            }   
+             
+        }
+    }
+}
---
+++ twelfth_task/problem1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle.checkCoordinate(3,4,10,0,0);
+            Console.WriteLine(Circle.getLength(10));
+            Console.WriteLine(Circle.getSquare(10));
+            Console.ReadKey();
+        }
+    }
+}
