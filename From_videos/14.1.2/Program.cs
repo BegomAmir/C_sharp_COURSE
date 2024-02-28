632d5057f76c19ef817674ea712d581bab334069
Amir Talkanov <amir.talkanov@yandex.ru>
2024-02-28 12:08:41 +0300

    Добавить задачи из первого видео темы 14

---
+++ From_videos/14.1.2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    abstract class Figure
+    {
+        public abstract double getArea();
+        public abstract double getPerimeter();
+        public abstract string Name { get; set; }
+    }
+    class Rectangular : Figure
+    {
+        string name;
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public override double getArea()
+        {
+            return Width * Height;
+        }
+
+        public override double getPerimeter()
+        {
+            return 2 * (Width + Height);
+        }
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+}
---
+++ From_videos/14.1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Тут про виртуальные методы и свойства
+            RegularTriangle regularTriangle = new RegularTriangle();
+            Triangle triangle = regularTriangle;
+            if (triangle is RectangularTriangle)
+            {
+                RectangularTriangle rectangularTriangle = (RectangularTriangle)triangle;
+            }
+            RectangularTriangle rectangularTriangle1 = triangle as RectangularTriangle;
+            Console.ReadKey();
+        }
+    }
+    class Triangle
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        public Triangle()
+        {
+
+        }
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public double getArea()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+    class RectangularTriangle : Triangle
+    {
+        public RectangularTriangle(double a, double b)
+        {
+            A = a;
+            B = b;
+            C = Math.Sqrt(a * a + b * b);
+        }
+        public new double getArea()
+        {
+            return A * B / 2;
+        }
+    }
+    class RegularTriangle : Triangle
+    {
+
+    }
+}
