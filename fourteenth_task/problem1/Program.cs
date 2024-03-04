b8ef9576fbb179e76b91141a3486381cb14a9174
Amir Talkanov <amir.talkanov@yandex.ru>
2024-03-05 00:16:24 +0300

    Добавить решение задачи темы 14

---
+++ fourteenth_task/problem1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Муся");
+            Dog dog = new Dog("Жучка");
+            dog.ShowInfo();
+            cat.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
