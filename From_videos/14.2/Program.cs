5116ecd678b6db7dc356b24c7ab8a64cc0eba9e1
Amir Talkanov <amir.talkanov@yandex.ru>
2024-03-04 23:46:55 +0300

    Добавить задачу из второго видео темы 14

---
+++ From_videos/14.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Apple apple = new Apple("Антоновка", "Кислый", "Россия");
+            apple.showInfo();
+            Console.ReadKey();
+        }
+    }
+}
