18278f923be0532fd17f8c1dede06def579bf98a
Amir Talkanov <amir.talkanov@yandex.ru>
2024-03-09 15:29:11 +0300

    Добавить задачи из 15 темы

---
+++ From_videos/15.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AChiper achiper = new AChiper();
+            Console.WriteLine(achiper.Encode("ABC123 XYZ"));
+            BChiper bchiper = new BChiper();
+            Console.WriteLine(bchiper.Encode("ABC123 XYZ"));
+            Console.ReadKey();
+        }
+    }
+}
