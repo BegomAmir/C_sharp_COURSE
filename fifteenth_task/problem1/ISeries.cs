5ffdf63ed4adeb53e3f0724a071d3f2f242d7150
Amir Talkanov <amir.talkanov@yandex.ru>
2024-03-09 17:21:47 +0300

    Добавить решение из темы 15

---
+++ fifteenth_task/problem1/ISeries.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    interface ISeries
+    {
+        void setStart(int x);
+        int getNext();
+        void Reset();
+
+    }
+    class ArithProgression : ISeries
+    {
+        public int Step { get; set; }
+        public int CurrentValue { get; set; }
+        public int StartValue { get; set; }
+
+        public int getNext()
+        {
+            CurrentValue += Step;
+            return CurrentValue;
+        }
+
+        public void Reset()
+        {
+            CurrentValue = StartValue;
+        }
+
+        public void setStart(int x)
+        {
+            StartValue = x;
+            CurrentValue = StartValue;
+
+        }
+        public void setStep(int s)
+        {
+            Step = s;
+        }
+    }
+    class GeomProgression : ISeries
+    {
+        public int Step { get; set; }
+        public int CurrentValue { get; set; }
+        public int StartValue { get; set; }
+        public int getNext()
+        {
+            CurrentValue *= Step;
+            return CurrentValue;
+        }
+
+        public void Reset()
+        {
+            CurrentValue = StartValue;
+        }
+
+        public void setStart(int x)
+        {
+            StartValue = x;
+            CurrentValue = StartValue;
+        }
+        public void setStep(int s)
+        {
+            Step = s;
+        }
+    }
+}
---
+++ fifteenth_task/problem1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arithProgression = new ArithProgression();
+            arithProgression.setStart(2);
+            arithProgression.setStep(3);
+            Console.WriteLine(arithProgression.getNext());
+            arithProgression.Reset();
+            GeomProgression ge = new GeomProgression();
+            ge.setStart(2);
+            ge.setStep(3);
+            Console.WriteLine(ge.getNext());
+            Console.ReadKey();
+            
+        }
+    }
+   
+}
