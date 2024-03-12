42f189f23a58c7382a60fb3e58ead8fa287e4c4e
Amir Talkanov <amir.talkanov@yandex.ru>
2024-03-12 18:53:16 +0300

    Добавить решение задачи из самостоятельной работы темы 16

---
+++ sixtenth_task/problem1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.IO;
+
+namespace problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            Product[] product = new Product[n];
+                for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введите номер товара");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите название товара");
+                string name = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Введите стоимость товара");
+                int price = Convert.ToInt32(Console.ReadLine());
+                product[i] = new Product { Id = id, Name = name, Price = price };
+            }
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(product, options);
+            using(StreamWriter  sw = new StreamWriter("../../../Product.json"))
+            {
+                sw.WriteLine(jsonString);
+            }
+        }
+    }
+}
---
+++ sixtenth_task/problem2/Program.cs
@@ -0,0 +1,34 @@
+﻿using problem1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace problem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = String.Empty;
+            using (StreamReader sr = new StreamReader("../../../Product.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Product[] products = JsonSerializer.Deserialize < Product[]>(jsonString);
+            Product maxProduct = products[0];
+            foreach(Product p in products)
+            {
+                if(p.Price > maxProduct.Price)
+                {
+                    maxProduct = p;
+                }
+            }
+            Console.WriteLine($"{maxProduct.Name} имеет наибольшую цену");
+            Console.ReadKey();
+        }
+    }
+}
