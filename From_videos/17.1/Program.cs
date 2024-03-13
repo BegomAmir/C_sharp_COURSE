58a6cf3414ad37aba1b404ae7cd7467c06f0fc0e
Amir Talkanov <amir.talkanov@yandex.ru>
2024-03-13 14:11:48 +0300

    Добавить решения задач из темы 17

---
+++ From_videos/17.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Apartment<int, int> apartment1 = new Apartment<int, int>();
+            //apartment1.Num = 1;
+
+            //Apartment<string, int> apartment2 = new Apartment<string, int>();
+            //apartment2.Num = "01";
+            print<int>(12);
+            print<string>("hello!");
+            Console.ReadKey();
+        }
+        static void print<T>(T value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+    //class Apartment<T, U>
+    //{
+    //    public U Level { get; set; }
+    //    public T Num { get; set; }
+    //}
+}
---
+++ From_videos/17.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using _16._2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите числовой номер");
+            //int numInt = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите имя");
+            //string name1 = Console.ReadLine();
+            //Console.WriteLine("Введите зп");
+            //int summa1 = Convert.ToInt32(Console.ReadLine());
+
+            //Empoyee<int> empoyee1 = new Empoyee<int>(numInt, name1, summa1);
+            //Console.WriteLine("Введите строковый номер");
+            //string numString = Console.ReadLine(); ;
+            //Console.WriteLine("Введите имя");
+            //string name2 = Console.ReadLine();
+            //Console.WriteLine("Введите зп");
+            //int summa2 = Convert.ToInt32(Console.ReadLine());
+            //Empoyee<string> empoyee2 = new Empoyee<string>(numString, name2, summa2);
+            //Console.WriteLine(empoyee1.GetInfo());
+            //Console.ReadKey();
+
+            Empoyee<int> empoyee1 = new Empoyee<int>();
+            empoyee1.Input();
+            Console.WriteLine(empoyee1.GetInfo());
+
+            Empoyee<string> empoyee2 = new Empoyee<string>();
+            empoyee2.Input();
+            Console.WriteLine(empoyee2.GetInfo());
+
+            Console.ReadKey();  
+
+        }
+    }
+}
