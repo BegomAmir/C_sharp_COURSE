843b080e4d86e3ce34b48de7521187b15224508f
Amir Talkanov <amir.talkanov@yandex.ru>
2024-03-13 15:08:16 +0300

    Добавить решение самостоятельной задачи из темы 17

---
+++ seventeenth_task/problem1/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    internal class BankAccount <T>
+    {
+        T AccountId {  get; set; }
+        int Balance { get; set; }
+        string Name { get; set; }
+        public void Input()
+        {
+            Console.WriteLine("Введите номер аккаунта");
+            AccountId = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите уровень баланса");
+            Balance = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите имя владельца");
+            Name = Console.ReadLine();  
+        }
+        public void Output()
+        {
+            Console.WriteLine($"{AccountId} {Name} {Balance}");
+        }
+    }
+}
---
+++ seventeenth_task/problem1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> account1 = new BankAccount<int>();
+            account1.Input();
+            account1.Output();
+            BankAccount<string> account2 = new BankAccount<string>();
+            account2.Input();
+            account2.Output();
+            Console.ReadKey();
+        }
+    }
+}
