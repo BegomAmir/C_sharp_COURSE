6d1ca0fdca6dec2eecdab24eeb9e6f05b922adfc
Amir Talkanov <amir.talkanov@yandex.ru>
2024-03-16 13:44:43 +0300

    Добавить решение задач из темы 21

---
+++ From_videos/21.1.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _21._1._2
+{
+    internal class Program
+    {
+        static int x = 0;
+        static object locker = new object();
+        public static void Print()
+        {
+            lock (locker)
+                {
+                    x = 1;
+                for (int i = 0; i < 9; i++)
+                {
+                    Console.WriteLine("{0}: {1}", Thread.CurrentThread.Name, x);
+                    x++;
+                    Thread.Sleep(100);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                ThreadStart threadStart = new ThreadStart(Print); 
+                Thread myThread = new Thread(threadStart);  
+                myThread.Name = "Поток " + i.ToString();
+                myThread.Start();
+            }
+            Console.ReadLine();
+        }
+    }
+}
