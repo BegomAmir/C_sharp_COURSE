5f33d24a023f0d3e0d0de51abbbc2d3a2aae50a2
Amir Talkanov <amir.talkanov@yandex.ru>
2024-03-18 15:54:20 +0300

    Добавить решение самостоятельной задачи темы 23

---
+++ twentythird_task/problem1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+            SumAsync(n);
+            Console.ReadKey();
+        }
+        static void Multiply(int n)
+        {
+            int s = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                s *= i;
+            }
+            Console.WriteLine(s);
+
+        }
+        static async void SumAsync(int n)
+        {
+            await Task.Run(() => Multiply(n));
+        }
+    }
+}
